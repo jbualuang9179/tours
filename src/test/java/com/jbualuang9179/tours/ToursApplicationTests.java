3c7ec605a44ad0622b9e9210af7748ad36a48366
Jirapong Bualuang <jbualuang21@gmail.com>
2025-09-29 22:09:37 +0700

    first commit

---
+++ src/test/java/com/jbualuang9179/tours/ToursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbualuang9179.tours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
